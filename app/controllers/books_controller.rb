1917cbb628fba6e8fad4dda465cf9567aa33a1f6
EC2 Default User <ec2-user@ip-172-31-17-129.ap-northeast-1.compute.internal>
2022-09-05 12:18:14 +0000

    [Finish] 課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -32,9 +35,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
@@ -42,5 +45,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user == current_user
+      redirect_to books_path
+    end
   end
 end
